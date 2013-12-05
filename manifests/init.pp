6b0737968c8ac3a5d0f92b9b8c7b33980fc526b1
Paul Canham <pcanham@cambridge.org>
2013-12-05 08:14:58 +0000

    adding new improved motd with ability to add text from local machine with /etc/motd.local

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,14 +11,38 @@
 # Sample Usage:
 #
 class motd {
-  if $::kernel == 'Linux' {
-    file { '/etc/motd':
-      ensure  => file,
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      backup  => false,
-      content => template('motd/motd.erb'),
-    }
+
+  concat{'/etc/motd':
+    owner => root,
+    group => root,
+    mode  => 0644
   }
+
+  concat::fragment{'motd_header':
+    target => '/etc/motd',
+    content => template('motd/motd.erb'),
+    order   => 01,
+  }
+
+  # local users on the machine can append to motd by just creating
+  # /etc/motd.local
+  concat::fragment{'motd_local':
+    target => '/etc/motd',
+    ensure  => '/etc/motd.local',
+    order   => 15
+  }
+
+	# used by other modules to register themselves in the motd
+	define motd::register($content="", $order=10) {
+	  if $content == "" {
+	    $body = $name
+	  } else {
+      $body = $content
+    }
+
+    concat::fragment{"motd_fragment_$name":
+	    target  => '/etc/motd',
+      content => "    -- $body\n"
+    }
+	}
 }
